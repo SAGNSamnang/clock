1a40b9ce6e961226088564d612571b7dc558c646
SAGNSamnang <110118114+SAGNSamnang@users.noreply.github.com>
2023-12-15 17:48:00 +0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+const numberHours = document.querySelector('.number-hours');
+const barSeconds = document.querySelector(".bar-seconds");
+
+const numberelement = [];
+
+const barelement = [];
+
+for (let i = 1; i<=12; i++) {
+        numberelement.push(`<span style="--index:${i};"><p>${i}</p></span>`);
+}
+numberHours.insertAdjacentHTML("afterbegin", numberelement.join(""));
+
+for (let i=1; i<=60; i++) {
+        barelement.push(`<span style="--index:${i};"><p></p></span>`);
+}
+barSeconds.insertAdjacentHTML("afterbegin", barelement.join(""));
+
+
+const handHours = document.querySelector(".hand.hours");
+const handMinutes = document.querySelector(".hand.minutes");
+const handSeconds = document.querySelector(".hand.seconds");
+
+function getCurrentTime() {
+        let date = new Date();
+        let currentHours = date.getHours();
+        let currentMinutes = date.getMinutes();
+        let currentSeconds = date.getSeconds();
+
+        handHours.style.transform =  `rotate(${currentHours * 30 + currentMinutes / 2}deg)`
+
+        handMinutes.style.transform = `rotate(${currentMinutes * 6}deg)`;
+
+        handSeconds.style.transform = `rotate(${currentSeconds * 6}deg)`;
+}
+getCurrentTime();
+setInterval(getCurrentTime, 1000);
